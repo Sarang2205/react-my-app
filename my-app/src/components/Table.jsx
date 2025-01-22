82d84251cfc06ebf40ca1491380deb784e869360
sarang <sarangshinde32@gmail.com>
2025-01-22 09:29:07 +0530

    activity

--- my-app/src/components/Table.jsx
+++ my-app/src/components/Table.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import TableHeader from "./TableHeader"
+import TableBody from "./TableBody"
+import './Table.css'
 export const Table = () => {
   return (
-    <div>Table</div>
+    <div>
+        <table className="one">
+            <TableHeader />
+            <TableBody />
+        </table>
+    </div>
   )
 }
+export default Table
